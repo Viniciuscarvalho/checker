421ee3102f0b299f903443a3dbeabefa8b67e955
viniciuscarvalho <viniciuscarvalho789@gmail.com>
2016-09-01 22:50:13 -0300

    Changes on pods, mutable eventsListViewController and Model

--- Checker/Checker/Event.swift
+++ Checker/Checker/Event.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2016 Vinicius Carvalho. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import ObjectMapper
+
+struct Event: Mappable {
+
+    var id: Int!
+    var name: String?
+    var time: String?
+    
+    init?(_ map: Map) {}
+    
+    mutating func mapping(map: Map) {
+    
+        id <- map["id"]
+        name <- map["name"]
+        time <- map["time"]
+    }
+    
+}
---
+++ Checker/Checker/EventsListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EventsListViewController.swift
+//  Checker
+//
+//  Created by Vinicius Carvalho on 01/09/16.
+//  Copyright © 2016 Vinicius Carvalho. All rights reserved.
+//
+
+import UIKit
+
+class EventsListViewController : UIViewController {
+
+    //MARK: Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Eventos"
+    }
+
+}
